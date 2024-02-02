530108edc9929eeee06f52cccc050d675fad848d
Jose Sandoval <Software.Sandoval@gmail.com>
2024-02-01 23:06:44 -0600

    monkeypatch works

--- test_calculator.py
+++ test_calculator.py
@@ -28,24 +28,26 @@ def test_terminal_output2(capsys):
     captured = capsys.readouterr()
     assert captured.out == "Result: 0\n"
 
-def test_argument_passing():
-    # monkeypatch.setattr("sys.argv", ["python3", "calculator.py", "6", "2", "divide"])
-    # print("test start")
-    result = subprocess.run(["python3" ,"calculator.py", '6', '2', "divide"], capture_output=True, text=True)
-    print(result.returncode)
-    print(result.stdout)
-    print(result.stderr)
-    # captured = capsys.readouterr()
-    # print(captured)
-    assert result.stdout == "Return: 3.0\n"
-    # assert calculator.calculate(6, 2, "divide") == 3.0
-    # num1 = float(calculator.sys.argv[1])
-    # num2 = float(calculator.sys.argv[2])
-    # operation = calculator.sys.argv[3]
+def test_argument_passing(monkeypatch):
+    # # monkeypatch.setattr("sys.argv", ["python3", "calculator.py", "6", "2", "divide"])
+    # # print("test start")
+    # result = subprocess.run(["python3" ,"calculator.py", '6', '2', "divide"], capture_output=True, text=True)
+    # print(result.returncode)
+    # print(result.stdout)
+    # print(result.stderr)
+    # # captured = capsys.readouterr()
+    # # print(captured)
+    # assert result.stdout == "Return: 3.0\n"
+    # # assert calculator.calculate(6, 2, "divide") == 3.0
+    # # num1 = float(calculator.sys.argv[1])
+    # # num2 = float(calculator.sys.argv[2])
+    # # operation = calculator.sys.argv[3]
 
-    # result = calculator.calculate(num1, num2, operation)
-    # print(f"Result: {result}")
-    
+    # # result = calculator.calculate(num1, num2, operation)
+    # # print(f"Result: {result}")
+    monkeypatch.setattr("sys.argv", ['python3',"calculator.py", "6", "2", "divide"]) 
+    process = subprocess.run(sys.argv, capture_output=True, text=True)
+    assert process.stdout == "Return: 3.0\n"
 
    # Add more tests to cover edge cases and negative scenarios
 
